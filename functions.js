d8bad4a5abdd84228c69b40aa25a3b6fa49c03e4
Sam Dyson <samdysonz@gmail.com>
2015-10-25 13:23:10 +1300

    Add JS file

---
+++ functions.js
@@ -0,0 +1,19 @@
+> function cube (x) {
+... return x * x;
+... }
+undefined
+> cube(3)
+9
+> cube(5)
+25
+
+> function absolute (x) {
+... if (x > 0) {
+..... return (x)
+..... } else {
+..... return (x * -1)
+..... }
+... }
+undefined
+> absolute (-4)
+4
